4402d5aa8b36bed82c26e4b35965e42103513c03
Turbo34 <adnanghelle@outlook.com>
2024-05-21 01:04:58 +0200

    lets do this

--- lib/screens/views/standings_view/standings.dart
+++ lib/screens/views/standings_view/standings.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:court_craze/json/jsons.dart';
